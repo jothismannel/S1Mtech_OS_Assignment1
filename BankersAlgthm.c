62c4cbc81e019e487637d6ae987ed21bb82ddcc0
jothismannel <jothisreghu@gmail.com>
2012-10-19 14:58:26 +0530

    Update BankersAlgthm.c

--- BankersAlgthm.c
+++ BankersAlgthm.c
@@ -1,4 +1,3 @@
-
 /*Program to implement bankers algorithm
  * input=number of resoures,number of process
  * output = determine whether the state is safe
@@ -6,151 +5,112 @@
 	  	
 	
 #include<stdio.h>
-	 
-main()
-{  
-	int m,n;            //n-->number of processes
-	int i,j,flagchek=0;   //m-->number of  resource types
-	 
-	printf("enter the number of process: ");  
-	
-	printf("\n***************************\n");
-	scanf("%d",&n);  
-	
-	printf("enter the number of resource types: ");
-	
-	printf("\n**********************************\n");
-	scanf("%d",&m);
-	  
-	 
-	int avail[1][20],max[20][20],alloc[20][20],
-             need[20][20],finish[10];
-	 
-	int seq[10],flagarr[10];
-	
-	printf("enter the values for the matrix max \n");
-	printf("\n**********************************\n");
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<m;j++)
-		{
-	            printf("\nvalue for position %d and %d",i,j);
-	            scanf("%d",&max[i][j]);
-	        }
-	    
-	 }
-	    
-	 printf("enter the values for the allocation matrix :\n");
-	 printf("\n******************************************\n");
-	 for(i=0;i<n;i++)
-	 {
-	       for(j=0;j<m;j++)
-	       {
-	           printf("\nvalue for position %d and %d",i,j);
-	           scanf("%d",&alloc[i][j]);
-	       }
-	    
-	 }
-	    
-	 printf("enter the number of available resources :\n");
-	 printf("\n***************************************\n");
-	 
-	 for(i=0;i<m;i++)
-	 {
-	          printf("\nvalue for position %d",i);
-	          scanf("%d",&avail[0][i]);
-	 }
-	 for(i=0;i<n;i++)
-	 {
-             for(j=0;j<m;j++)
-                {
-                   need[i][j]=max[i][j]-alloc[i][j];
-                }
-          }
 
-	  for(i=0;i<n;i++)
-	  {
-	      finish[i]=0;
-	  }
-	    int k;
-	    int x=0;
-	 
-	   
-	  for(i=0;i<n;i++)
-	  {
-	        for(j=0;j<m;j++)
-	        {
-	           if(finish[i]==0 && 
-		   need[i][j]<=avail[0][j])
-	           {
-	               flagarr[j]=1;
-                   }
-	 
-	           else
-	           {
- 
-	           flagarr[j]=0;
-	           }
-	  }
-	  int flag=0;
-	  int flagarrint=0;
-	  
-           for(k=0;k<m;k++)
-           {
-                if(flagarr[k]==1)
+int main()
+{
+    int claim[7][5],request[7][5],allocate[7][5],resource[5],available[5],comp[7];
+    int first,p,r,i,j,prc,count,t;
+    count=0;
+    for(i=1;i<=7;i++)
+        comp[i]=0;
+
+    printf("\n BANKERS ALGORITHM \n");
+    printf("Enter the no of processes : "); //reading number of process
+    scanf("%d",&p);
+    printf("\nEnter the no of resources : "); //reading number of resourses
+    scanf("%d",&r);
+    printf("\nEnter the claim for each process : ");
+    for(i=1;i<=p;i++)
+    {
+        printf("\nFor process %d : ",i);
+        for(j=1;j<=r;j++)
+        {
+            scanf("%d",&claim[i][j]);
+        }
+    }
+    printf("\nEnter the allocation for each process : ");
+    for(i=1;i<=p;i++)
+    {
+        printf("\nFor process ",i);
+        for(j=1;j<=r;j++)
+        {
+            scanf("%d",&allocate[i][j]);
+        }
+    }
+    printf("\nEnter total no of each resource : ");
+    for(j=1;j<=r;j++)
+        scanf("%d",&resource[j]);
+    for(j=1;j<=r;j++)
+    {
+        int total=0;
+        avail[j]=0;
+        for(i=1;i<=p;i++)
+        {
+	total+=allocate[i][j];
+	}
+        available[j]=resource[j]-total;
+    }
+    do
+    {
+        for(i=1;i<=p;i++)
+        {
+            for(j=1;j<=r;j++)
+            {
+                request[i][j]=claim[i][j]-allocate[i][j];
+            }
+        }
+        printf("\n\nAvailable resources are : ");
+        for(j=1;j<=r;j++)
+        { printf(" ",available[j]); }
+        printf("\nClaim matrix:\tAllocation matrix:\n");
+        for(i=1;i<=p;i++)
+        {
+            for(j=1;j<=r;j++)
+            {
+                printf("%d\t",claim[i][j]);
+            }
+            printf("\t\t\t");
+            for(j=1;j<=r;j++)
+            {
+                printf("%d\t",allocate[i][j]);
+            }
+            printf("\n");
+        }
+        prc=0;
+        for(i=1;i<=p;i++)
+        {
+            if(comp[i]==0)
+            {
+                prc=i;
+                for(j=1;j<=r;j++)
                 {
-                    flagarrint++;
-	 
+                    if(available[j])
+                    {
+                        prc=0;
+                        break;
+                    }
                 }
-	 
-	       if(flagarrint==m)
-               {   
-			for(k=0;k<m;k++)
-                        {
-	                        avail[0][k]+=alloc[i][k];
-	                        printf("%d",avail[0][k]);
-	                        finish[i]=1;
-	                        flag=1;
-	                 }
-	 
-	 
-	       }
-	 
-	       else
-	         finish[i]=0;
-	       }
-	   
-	   if(flag==1)
-	   {
-	        seq[x]=i;
-	         x++;
-	   }
-	   printf("\n");
-	 
-	  }
-	 
-	  for(i=0;i<n;i++)
-	  {
-	      if(finish[i]==1)
-	      {
-	           flagchek++;
-	 
-	      }
-	 }
-	      
-	 if(flagchek==n)
-	 {
-	 printf("The system is in the safe state");
-         }
-	 else
-	 {               
-	 printf("the process sequence is :<");
-	 }
-	 for(i=0;i<n;i++)
-	 {
-	         printf("%d",seq[i]);
-	 }
-	 printf(">\n");
-	 
-	  
-	 }
+            }
+            if(prc!=0)
+                break;
+        }
+        if(prc!=0)
+        {
+            printf("\nProcess ",prc,"runs to completion!");
+            count++;
+            for(j=1;j<=r;j++)
+            {
+                available[j]+=allocate[prc][j];
+                allocate[prc][j]=0;
+                claim[prc][j]=0;
+                comp[prc]=1;
+            }
+        }
+    }
+    while(count!=p&&prc!=0);
+    if(count==p)
+        printf("\nThe system is in a safe state!!");
+    else
+        printf("\nThe system is in an unsafe state!!");
+return ;
+}
